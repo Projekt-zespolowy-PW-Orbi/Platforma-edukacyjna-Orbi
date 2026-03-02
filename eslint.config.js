9c2ada131d26d2bb191dd41205a3fb9de0755501
Kamil Nowocień <kamil.nowocien.stud@pw.edu.pl>
2026-03-02 11:06:17 +0100

    chore: setup React project with ESLint and Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,12 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -12,12 +15,22 @@ export default defineConfig([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      react.configs.flat.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    settings: {
+      react: {
+      version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
     },
   },
 ])
